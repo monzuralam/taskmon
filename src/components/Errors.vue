653a3f712db20f6ce92ce40b7688ba4cb3779830
monzuralam <monzur707@gmail.com>
2023-09-21 14:40:16 +0600

    Error page structure has been update

--- src/components/Errors.vue
+++ src/components/Errors.vue
@@ -1,13 +1,18 @@
 <template>
-  <section class="relative py-4 md:py-10">
+  <Topbar />
+  <Menu />
+  <div class="fixed pl-80 w-full min-h-full flex justify-center items-center">
     <div class="text-center">
       <h1 class="text-6xl underline">404</h1>
       <p class="text-xl">not found.</p>
     </div>
-  </section>
+  </div>
 </template>
 <script>
+import Topbar from "./Topbar.vue";
+import Menu from "./Menu.vue";
 export default {
   name: "Errors",
+  components: { Topbar, Menu },
 };
 </script>
