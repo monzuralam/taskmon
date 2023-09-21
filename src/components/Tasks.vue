4f8d94fee47944cb4babedf62ab9341b0848ce29
monzuralam <monzur707@gmail.com>
2023-09-21 14:47:53 +0600

    Tasks component has been added

---
+++ src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+  <Topbar />
+  <Menu />
+  <!-- content start here -->
+  <div class="fixed top-20 pl-80 w-full">
+    <h2>Tasks</h2>
+  </div>
+  <!-- content stop here -->
+</template>
+<script>
+import Topbar from "./Topbar.vue";
+import Menu from "./Menu.vue";
+export default {
+  name: "Tasks",
+  components: { Topbar, Menu },
+};
+</script>
