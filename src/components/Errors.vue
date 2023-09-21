2fb7e98caa96c1a0feef5686c1be0d9452df019e
monzuralam <monzur707@gmail.com>
2023-09-21 10:16:37 +0600

    404 component has been added

---
+++ src/components/Errors.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="relative py-4 md:py-10">
+    <div class="text-center">
+      <h1 class="text-6xl underline">404</h1>
+      <p class="text-xl">not found.</p>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "Errors",
+};
+</script>
