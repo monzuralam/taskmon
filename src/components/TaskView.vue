e9a5e3f3569b334df596d0d8b61931959c2b930e
monzuralam <monzur707@gmail.com>
2023-09-22 08:07:19 +0600

    task view component has been added

---
+++ src/components/TaskView.vue
@@ -0,0 +1,17 @@
+<template>
+  <Topbar />
+  <Menu />
+  <!-- content start here -->
+  <div class="fixed top-20 pl-80 w-full">
+    <h2 class="text-3xl font-semibold mb-4">Task Details</h2>
+  </div>
+  <!-- content stop here -->
+</template>
+<script>
+import Topbar from "./Topbar.vue";
+import Menu from "./Menu.vue";
+export default {
+  name: "TaskView",
+  components: { Topbar, Menu },
+};
+</script>
