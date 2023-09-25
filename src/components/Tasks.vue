1798b4d3c3eafe296f6c9d431851fa807f736bc7
monzuralam <monzur707@gmail.com>
2023-09-25 07:06:02 +0600

    add task button has been added

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -3,7 +3,16 @@
   <Menu />
   <!-- content start here -->
   <div class="fixed top-20 pl-80 w-full">
-    <h2 class="text-3xl font-semibold mb-4">Tasks</h2>
+    <div class="flex justify-between md:pr-6">
+      <div><h2 class="text-3xl font-semibold mb-4">Tasks</h2></div>
+      <div>
+        <router-link
+          to="/task-add/"
+          class="text-lg bg-black text-white px-2 py-1 border rounded"
+          >Add Task</router-link
+        >
+      </div>
+    </div>
     <TasksList />
   </div>
   <!-- content stop here -->
