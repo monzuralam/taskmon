47554770e257d8b345eeee9908e321b223c61046
monzuralam <monzur707@gmail.com>
2023-09-25 08:23:20 +0600

    AddTask component has been added

---
+++ src/components/AddTask.vue
@@ -0,0 +1,19 @@
+<template>
+  <Topbar />
+  <Menu />
+  <!-- content start here -->
+  <div class="fixed top-20 pl-80 w-full">
+    <TasksTopbar />
+    <AddTaskForm />
+  </div>
+  <!-- content stop here -->
+</template>
+<script>
+import Topbar from "./Topbar.vue";
+import Menu from "./Menu.vue";
+import TasksTopbar from "./Tasks/TasksTopbar.vue";
+export default {
+  name: "Tasks",
+  components: { Topbar, Menu, TasksTopbar },
+};
+</script>
