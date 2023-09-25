9116c20b26b24320244488e44483f6fb0a668b7f
monzuralam <monzur707@gmail.com>
2023-09-25 08:05:53 +0600

    taskstop bar component has been created

---
+++ src/components/Tasks/TasksTopbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-between md:pr-6">
+    <div><h2 class="text-3xl font-semibold mb-4">Tasks</h2></div>
+    <div>
+      <router-link
+        to="/task-add/"
+        class="text-lg bg-black text-white px-2 py-1 border rounded"
+        >Add Task</router-link
+      >
+    </div>
+  </div>
+</template>
